f86fd50144fc70cda24d30feafeed2eab576a830
pixotchi <pixotchi@proton.me>
2024-07-04 23:51:43 +0400

    Add new dependencies and providers, update app layout
    Added new dependencies to package.json for the Privy, Wagmi, react-query and viem libraries. Introduced a new file, providers.tsx, which sets up the necessary providers for Privy and Wagmi. Also updated the title and description in app layout. Updated package-lock.json to reflect these new dependencies.

---
+++ src/app/providers.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {http, webSocket} from 'viem';
+import {baseSepolia} from 'viem/chains';
+
+import type { PrivyClientConfig} from '@privy-io/react-auth';
+import {addRpcUrlOverrideToChain} from '@privy-io/react-auth';
+import {PrivyProvider} from '@privy-io/react-auth';
+import {WagmiProvider, createConfig} from '@privy-io/wagmi';
+import {fallback} from "wagmi";
+
+const queryClient = new QueryClient();
+
+export const wagmiConfig = createConfig({
+  chains: [baseSepolia],
+  transports: {
+    [baseSepolia.id]: fallback([
+      webSocket(process.env.NEXT_PUBLIC_RPC_SERVER_WS),
+      http(process.env.NEXT_PUBLIC_RPC_SERVER),
+    ])
+  },
+});
+
+const baseSepoliaOverride = addRpcUrlOverrideToChain(baseSepolia, process.env.NEXT_PUBLIC_RPC_SERVER!);
+
+const privyConfig: PrivyClientConfig = {
+  embeddedWallets: {
+    createOnLogin: 'users-without-wallets',
+    requireUserPasswordOnCreate: false,
+    noPromptOnSignature: false,
+  },
+  loginMethods: ['wallet', 'email', 'sms'],
+  appearance: {
+    showWalletLoginFirst: true,
+  },
+
+};
+
+export default function Providers({children}: {children: React.ReactNode}) {
+  return (
+    <PrivyProvider
+      /*apiUrl={process.env.NEXT_PUBLIC_PRIVY_AUTH_URL as string}*/
+      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID as string}
+      config={{
+        ...privyConfig,
+        defaultChain: baseSepolia,
+        supportedChains: [baseSepoliaOverride],
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <WagmiProvider config={wagmiConfig} reconnectOnMount={false}>
+          {children}
+        </WagmiProvider>
+      </QueryClientProvider>
+    </PrivyProvider>
+  );
+}
