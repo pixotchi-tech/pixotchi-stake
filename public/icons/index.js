b8861cb88dd5cb0bd0f7203ba381e44ec8297619
blockdeveloper57 <pixotchi@proton.me>
2024-07-12 14:07:00 +0330

    icons

--- public/icons/index.js
+++ public/icons/index.js
@@ -1,5 +1,7 @@
 import BtnBlue from "./BtnBlue.svg"
 import BtnNavy from "./BtnNavy.svg"
 import Logo from "./logo.png"
+import SeedIcon from "./SEED.png"
+import leafIcon from "./leaf.png"
 
-export { BtnBlue, BtnNavy, Logo }
+export { BtnBlue, BtnNavy, Logo, SeedIcon, leafIcon }
